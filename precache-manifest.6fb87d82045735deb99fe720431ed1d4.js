7639b22f8eb42595da893d228e2465ac04a04733
karanbaboota <karanbaboota@gmail.com>
2022-10-13 19:10:19 +0530

    Updates

---
+++ precache-manifest.6fb87d82045735deb99fe720431ed1d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "85fcdb5c838978780a88",
+    "url": "/karan20210/Portfolio/static/css/main.8b8639f5.chunk.css"
+  },
+  {
+    "revision": "85fcdb5c838978780a88",
+    "url": "/karan20210/Portfolio/static/js/main.85fcdb5c.chunk.js"
+  },
+  {
+    "revision": "1e6fddff280d51462bd7",
+    "url": "/karan20210/Portfolio/static/js/runtime~main.1e6fddff.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/karan20210/Portfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "937c997a4e099e0a9bbeada8a83f9567",
+    "url": "/karan20210/Portfolio/index.html"
+  }
+];
